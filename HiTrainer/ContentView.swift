11dc6416133071fb0bc612b1db8da58952dcc03d
Thierry Basset <thierrybasset@free.fr>
2020-05-18 21:40:06 +0200

    Initial Commit

---
+++ HiTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HiTrainer
+//
+//  Created by Thierry BASSET on 18/05/2020.
+//  Copyright © 2020 Thierry BASSET. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
